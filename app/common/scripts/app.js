73dde9c311240b4120b1d173bbeea420742d649e
Елизавета Хорошева <eliz@email.com>
2019-08-27 15:35:36 +0300

    added javascript

---
+++ app/common/scripts/app.js
@@ -0,0 +1,13 @@
+// const test = () => "Hello, world";
+
+// console.log(test());
+
+var title = document.getElementById('title');
+
+function showHide() {
+  if (title.style.display != 'block') { // если блок скрыт
+    title.style.display = 'block'; // открываем его
+  } else { // если открыт
+    title.style.display = 'none'; // скрываем
+  }
+}
